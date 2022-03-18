6d7c9e32f3b66227654fc84a8e88a1e25be20043
Muttakin-shikder <99129135+Muttakin-Shikder@users.noreply.github.com>
2022-03-17 20:51:34 -0700

    Update binary-cal-con-cli.py

--- binary-cal-con-cli.py
+++ binary-cal-con-cli.py
@@ -1,45 +1,44 @@
 print('''
 1. know about numbers binary.
 2. calculation in binary.
-> Exit (press the (ctrl + ^c / cmd+^c) from the keyboard).
+3. Exit.
 ''')
 select=int(input("input your option: "))
 
-fb=int(input("input your first binary number: "))
-
 if select == 1:
+
+    fb=int(input("input your first binary number: "))
 
     print(bin(fb)+" this binary of ",fb)
 
 elif select==2:
 
     print("1.+ 2.- 3./ 4.*")
-
     aritop=int(input("input your arithm..: "))
+    fb=int(input("input your first binary number: "))
+
 
     if aritop==1:
-
+        # fb=int(input("input your first binary number: "))
         sb=int(input("input your second binary number: "))
 
         print(bin(sb+fb)+" this binary of ",sb+fb)
 
     if aritop==2:
-
+        # fb=int(input("input your first binary number: "))
         sb=int(input("input your second binary number: "))
 
         print(bin(sb-fb)+" this binary of ",sb+fb)
 
     if aritop==3:
-
+        # fb=int(input("input your first binary number: "))
         sb=int(input("input your second binary number: "))
 
         print(bin(sb/fb)+" this binary of ",sb+fb)
 
     if aritop==4:
-
+        # fb=int(input("input your first binary number: "))
         sb=int(input("input your second binary number: "))
-
         print(bin(sb*fb)+" this binary of ",sb+fb)
 else:
-    print("you are exiting from the tool...(because your have written wrong option)")
     exit()
